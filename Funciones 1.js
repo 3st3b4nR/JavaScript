7b787f7b0aaacb8d000a2bfc4d6659e8ed0f514c
Esteban Roldán <esteban.roldanrm@gmail.com>
2024-11-24 15:00:45 -0500

    Ejercicios de Funciones

---
+++ Funciones 1.js
@@ -0,0 +1,9 @@
+/*
+Funciones declarativas:
+Crea una función llamada saludar que tome un parámetro nombre y muestre un saludo en consola.
+*/
+
+function saludar(nombre)
+{console.log(`Hola ${nombre}`);}
+
+saludar("Esteban")
---
+++ Funciones 3.js
@@ -0,0 +1,9 @@
+/*
+Parámetros y valores de retorno:
+Crea una función que tome un número y devuelva su cuadrado.
+*/
+
+const cuadrado= function(numero)
+{return(numero**2);};
+
+console.log(`El cuadrado de 4 es ${cuadrado(4)}`);
